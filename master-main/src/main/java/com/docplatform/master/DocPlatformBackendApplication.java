e0180e19805ed2a0cbafe8810ed7c2e6a31b7e7b
GGBond <18273343478@163.com>
2026-04-15 23:43:23 +0800

    Update DocPlatformBackendApplication.java

--- master-main/src/main/java/com/docplatform/master/DocPlatformBackendApplication.java
+++ master-main/src/main/java/com/docplatform/master/DocPlatformBackendApplication.java
@@ -2,8 +2,12 @@ package com.docplatform.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class DocPlatformBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocPlatformBackendApplication.class, args);
