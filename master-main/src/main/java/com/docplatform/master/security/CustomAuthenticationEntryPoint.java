1c47743c48f69820993182812e75ba2967a246c8
GGBond <18273343478@163.com>
2026-04-16 16:07:18 +0800

    Create CustomAuthenticationEntryPoint.java

---
+++ master-main/src/main/java/com/docplatform/master/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package com.docplatform.master.security;
+
+import com.docplatform.master.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ResponseUtil.error(response, "Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
+}
