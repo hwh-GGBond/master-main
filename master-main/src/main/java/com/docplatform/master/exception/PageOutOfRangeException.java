18f79f0c49631a2aacc27b4fdb8f5cce3b26194a
GGBond <18273343478@163.com>
2026-04-16 18:01:40 +0800

    Create PageOutOfRangeException.java

---
+++ master-main/src/main/java/com/docplatform/master/exception/PageOutOfRangeException.java
@@ -0,0 +1,7 @@
+package com.docplatform.master.exception;
+
+public class PageOutOfRangeException extends RuntimeException {
+    public PageOutOfRangeException(String message) {
+        super(message);
+    }
+}
