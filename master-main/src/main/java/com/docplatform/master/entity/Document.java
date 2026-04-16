c3c90370a9b9851a2cfc735febf9467449837cd5
GGBond <18273343478@163.com>
2026-04-16 15:45:25 +0800

    Update Document.java

--- master-main/src/main/java/com/docplatform/master/entity/Document.java
+++ master-main/src/main/java/com/docplatform/master/entity/Document.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
@@ -38,6 +39,7 @@ public class Document {
     @Column(name = "converted", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
     private boolean converted;
     
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
