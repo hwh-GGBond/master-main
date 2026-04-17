06842b01e035fcc858ceb42bd0c1bf82dabc25ca
GGBond <18273343478@163.com>
2026-04-17 22:51:17 +0800

    Update AuthController.java

--- master-main/src/main/java/com/docplatform/master/controller/AuthController.java
+++ master-main/src/main/java/com/docplatform/master/controller/AuthController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -58,6 +60,26 @@ public class AuthController {
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(Map.of("error", "Internal server error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping("/token/validate")
+    public ResponseEntity<?> validateToken(Authentication authentication) {
+        try {
+            // 从 SecurityContext 中获取用户名
+            String username = authentication.getName();
+            
+            // 这里可以返回 token 相关信息
+            // 注意：实际的 token 有效期信息需要从请求头中解析
+            // 为了简化，我们返回配置的过期时间
+            Map<String, Object> response = new HashMap<>();
+            response.put("username", username);
+            response.put("tokenExpiration", "24 hours"); // 24小时
+            response.put("status", "valid");
+            
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(Map.of("error", "Invalid token"), HttpStatus.UNAUTHORIZED);
         }
     }
     
